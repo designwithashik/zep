56df291daa3f933e3193cc33e1c6c0f69472bb90
designwithashik <designwithashik@gmail.com>
2023-06-19 16:19:42 +0600

    Investors section added

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -17,15 +17,60 @@ const AboutPage = () => {
                 <img src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FGrowth.abe5b7d8.png&w=1920&q=100" alt="" />
             </section>
 
-            <section className=''>
-                <h3 className='text-2xl'>Got Featured</h3>
+            <section className='bg-[#FAFBFE]'>
+                <h3 className='text-[40px] font-bold' >Got Featured</h3>
                 <div className="flex justify-evenly">
-                    <img className='lg:max-w-[220px] md:max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FEconomicTimes.5724030e.png&w=640&q=75" alt="" />
-                    <img className='lg:max-w-[220px] md:max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInc42.7697aec8.png&w=640&q=75" alt="" />
-                    <img className='lg:max-w-[220px] md:max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmint.410f3a1a.png&w=640&q=75" alt="" />
-                    <img className='lg:max-w-[220px] md:max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FYourStory.1efabc81.png&w=640&q=75" alt="" />
-                    <img className='lg:max-w-[220px] md:max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBusinessStandard.047c592d.png&w=640&q=75" alt="" />
+                    <img className='lg:max-w-[220px] max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FEconomicTimes.5724030e.png&w=640&q=75" alt="" />
+                    <img className='lg:max-w-[220px] max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInc42.7697aec8.png&w=640&q=75" alt="" />
+                    <img className='lg:max-w-[220px]  max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmint.410f3a1a.png&w=640&q=75" alt="" />
+                    <img className='lg:max-w-[220px]  max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FYourStory.1efabc81.png&w=640&q=75" alt="" />
+                    <img className='lg:max-w-[220px] max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBusinessStandard.047c592d.png&w=640&q=75" alt="" />
                 </div>
+            </section>
+            <section>
+            <h3 className='text-[40px] font-bold'>Meet the Investors</h3>
+            <div className="flex justify-evenly">
+                    <img className='lg:max-w-[220px] max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInvCompany1.41d5c5bd.png&w=640&q=75" alt="" />
+                    <img className='lg:max-w-[220px] max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInvCompany2.3f604866.png&w=640&q=75" alt="" />
+                    <img className='lg:max-w-[220px] max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInvCompany3.c16095cb.png&w=640&q=75" alt="" />
+                    <img className='lg:max-w-[220px] max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInvCompany4.9acb56c2.png&w=640&q=75" alt="" />
+                    <img className='lg:max-w-[220px] max-w-[120px]' src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBusinessStandard.047c592d.png&w=640&q=75" alt="" />
+                </div>
+                <div className="grid grid-cols-4">
+                    <div className="card rounded-box">
+                    <img src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInv1.1c87bc0c.png&w=640&q=75" alt="" />
+                    </div>
+                    <div className="card rounded-box">
+
+                    <img src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInv2.331b31f9.png&w=640&q=75" alt="" />
+                    </div>
+                    <div className="card rounded-box">
+                        
+                    <img src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInv3.1779e0d4.png&w=640&q=75" alt="" />
+                    </div>
+                    <div className="card rounded-box">
+                        
+                    <img src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInv4.b9325fa0.png&w=640&q=75" alt="" />
+                    </div>
+                    <div className="card rounded-box">
+                        
+                    <img src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInv5.2e962217.png&w=640&q=75" alt="" />
+                    </div>
+                    <div className="card rounded-box">
+                        
+                    <img src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInv6.7a958b9d.png&w=640&q=75" alt="" />
+                    </div>
+                    <div className="card rounded-box">
+                        
+                    <img src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInv7.3b8a633d.png&w=640&q=75" alt="" />
+                    </div>
+                    <div className="card rounded-box">
+                    <img src="https://zetapp.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInv8.bcbe6091.png&w=640&q=75" alt="" />
+                        
+                    </div>
+                </div>
+
+
             </section>
         </div>
     );
